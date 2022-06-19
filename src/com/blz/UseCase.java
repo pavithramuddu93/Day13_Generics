adac0abf16d7712b09c017342390167980f7cc15
pavithramuddu93 <pavithramuddu93>
2022-06-19 22:10:16 +0530

    UC3 added

--- src/com/blz/UseCase.java
+++ src/com/blz/UseCase.java
@@ -5,7 +5,8 @@ public class UseCase {
     public static void main(String[] args) {
 
         // Integer xint = 65, yint = 85, zint = 22;
-        Float xf = 9.7f, yf=17.66f, zf = 5.5f;
+//        Float xf = 9.7f, yf=17.66f, zf = 55.5f;
+        String xs = "Pear", ys = "Apple", zs = "Orange";
 //        int max = xint;
 //        if (yint.compareTo(max) > 0) {
 //            max = yint;
@@ -14,14 +15,23 @@ public class UseCase {
 //            max = zint;
 //        }
 //        System.out.println(max + " is max number");
-        float max = xf;
-        if (yf.compareTo(max) > 0) {
-            max = yf;
-        }
-        if (zf.compareTo(max) > 0) {
-            max = zf;
-        }
-        System.out.println(max + " is max number");
+//       float maxt = xf;
+//        if (yf.compareTo(maxt) > 0) {
+//            maxt = yf;
+//        }
+//        if (zf.compareTo(maxt) > 0) {
+//            maxt = zf;
+//        }
+//        System.out.println(maxt + " is max number");
 
+        if ((ys.compareTo(xs) > 0) && (ys.compareTo(zs) > 0)) {
+            System.out.println(ys + " is max String");
+        }
+        if ((zs.compareTo(xs) > 0) && (zs.compareTo(ys) > 0)) {
+            System.out.println(zs + " is max String");
+        }
+        if ((xs.compareTo(ys) > 0) && (xs.compareTo(zs) > 0)) {
+            System.out.println(xs + " is max String");
+        }
     }
 }
