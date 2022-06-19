301408c2bbe5eb8fe70125abc44a286879743e7a
pavithramuddu93 <pavithramuddu93>
2022-06-19 22:15:56 +0530

    UC4 added

--- src/com/blz/UseCase.java
+++ src/com/blz/UseCase.java
@@ -1,37 +1,40 @@
 package com.blz;
 
 
-public class UseCase {
+import java.util.Arrays;
+import java.util.Collections;
+
+public class UseCase<T extends Comparable<T>> {
+    T x;
+    T y;
+    T z;
+    T r;
+
+    public UseCase(T x, T y, T z, T r) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.r = r;
+    }
+
     public static void main(String[] args) {
 
-        // Integer xint = 65, yint = 85, zint = 22;
-//        Float xf = 9.7f, yf=17.66f, zf = 55.5f;
-        String xs = "Pear", ys = "Apple", zs = "Orange";
-//        int max = xint;
-//        if (yint.compareTo(max) > 0) {
-//            max = yint;
-//        }
-//        if (zint.compareTo(max) > 0) {
-//            max = zint;
-//        }
-//        System.out.println(max + " is max number");
-//       float maxt = xf;
-//        if (yf.compareTo(maxt) > 0) {
-//            maxt = yf;
-//        }
-//        if (zf.compareTo(maxt) > 0) {
-//            maxt = zf;
-//        }
-//        System.out.println(maxt + " is max number");
+        Integer xint = 65, yint = 85, zint = 22, rint = 99;
+        new UseCase(xint, yint, zint, rint).testmaximum();
+        Float xf = 9.7f, yf = 17.66f, zf = 55.5f, rf = 69.4f;
+        new UseCase(xf, yf, zf, rf).testmaximum();
+        String xs = "Pear", ys = "Apple", zs = "Orange", rs = "Strawberry";
+        new UseCase(xs, ys, zs, rs).testmaximum();
+    }
 
-        if ((ys.compareTo(xs) > 0) && (ys.compareTo(zs) > 0)) {
-            System.out.println(ys + " is max String");
-        }
-        if ((zs.compareTo(xs) > 0) && (zs.compareTo(ys) > 0)) {
-            System.out.println(zs + " is max String");
-        }
-        if ((xs.compareTo(ys) > 0) && (xs.compareTo(zs) > 0)) {
-            System.out.println(xs + " is max String");
-        }
+
+    public static <T extends Comparable<T>> @nullable T testmaximum(T... a) {
+        Arrays.sort(a, Collections.reverseOrder());
+        System.out.println(a[0] + " is maximum");
+        return null;
+    }
+
+    public T testmaximum() {
+        return UseCase.testmaximum(x, y, z, r);
     }
 }
