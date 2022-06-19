c0572cbe3604e767c49d99231db81dc7cf76210d
pavithramuddu93 <pavithramuddu93>
2022-06-19 21:57:30 +0530

    RF1 added

--- src/com/blz/Main.java
+++ src/com/blz/Main.java
@@ -1,8 +1,29 @@
 package com.blz;
+public class Main<T> {
+    T Array[];
 
-public class Main {
+
+    Main(T[] Array) {
+        this.Array = Array;
+    }
+
 
     public static void main(String[] args) {
-	// write your code here
+        Integer intArray[] = {1, 2, 3};
+        Main printarray = new Main(intArray);
+        Main.toPrint(printarray.Array);
+        Double doubleArray[] = {1.223, 2.356, 3.456};
+        System.out.println();
+        Main printarray1 = new Main(doubleArray);
+        Main.toPrint(printarray1.Array);
+        Character charArray[] = {'A', 'b'};
+        System.out.println();
+        Main.toPrint(charArray);
+    }
+
+    public static <T> void toPrint(T[] Array) {
+        for (T value : Array) {
+            System.out.print(value + "  ");
+        }
     }
 }
